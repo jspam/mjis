8fe0b776cb01f02477c5e02eda3a4358418d583a
Max Wagner <max@trollbu.de>
2014-10-27 01:28:46 +0100

    Add literal parameter to TokenData, specifying it for each case class

--- src/main/scala/mjis/Token.scala
+++ src/main/scala/mjis/Token.scala
@@ -1,54 +1,54 @@
 package mjis
 
-abstract class TokenData {}
+abstract class TokenData(val literal: String) {}
 
 object TokenData {
 
   // Keyword case objects
-  case object BooleanType extends TokenData
-  case object Class extends TokenData
-  case object Else extends TokenData
-  case object False extends TokenData
-  case object If extends TokenData
-  case object IntType extends TokenData
-  case object New extends TokenData
-  case object Null extends TokenData
-  case object Public extends TokenData
-  case object Return extends TokenData
-  case object Static extends TokenData
-  case object This extends TokenData
-  case object True extends TokenData
-  case object VoidType extends TokenData
-  case object While extends TokenData
+  case object BooleanType extends TokenData("boolean")
+  case object Class extends TokenData("class")
+  case object Else extends TokenData("else")
+  case object False extends TokenData("false")
+  case object If extends TokenData("if")
+  case object IntType extends TokenData("int")
+  case object New extends TokenData("new")
+  case object Null extends TokenData("null")
+  case object Public extends TokenData("public")
+  case object Return extends TokenData("return")
+  case object Static extends TokenData("static")
+  case object This extends TokenData("this")
+  case object True extends TokenData("true")
+  case object VoidType extends TokenData("void")
+  case object While extends TokenData("while")
 
   // Non-alphanumeric Tokens
-  case object Unequal extends TokenData
-  case object Not extends TokenData
-  case object ParenOpen extends TokenData
-  case object ParenClosed extends TokenData
-  case object Mult extends TokenData
-  case object Plus extends TokenData
-  case object Comma extends TokenData
-  case object Minus extends TokenData
-  case object Dot extends TokenData
-  case object Divide extends TokenData
-  case object Semicolon extends TokenData
-  case object SmallerEquals extends TokenData
-  case object Smaller extends TokenData
-  case object Equals extends TokenData
-  case object Assign extends TokenData
-  case object GreaterEquals extends TokenData
-  case object Greater extends TokenData
-  case object Modulo extends TokenData
-  case object LogicalAnd extends TokenData
-  case object SquareBracketOpen extends TokenData
-  case object SquareBracketClosed extends TokenData
-  case object CurlyBraceOpen extends TokenData
-  case object CurlyBraceClosed extends TokenData
-  case object LogicalOr extends TokenData
+  case object Unequal extends TokenData("!=")
+  case object Not extends TokenData("!")
+  case object ParenOpen extends TokenData("(")
+  case object ParenClosed extends TokenData(")")
+  case object Mult extends TokenData("*")
+  case object Plus extends TokenData("+")
+  case object Comma extends TokenData(",")
+  case object Minus extends TokenData("-")
+  case object Dot extends TokenData(".")
+  case object Divide extends TokenData("/")
+  case object Semicolon extends TokenData(";")
+  case object SmallerEquals extends TokenData("<=")
+  case object Smaller extends TokenData("<")
+  case object Equals extends TokenData("==")
+  case object Assign extends TokenData("=")
+  case object GreaterEquals extends TokenData(">=")
+  case object Greater extends TokenData(">")
+  case object Modulo extends TokenData("%")
+  case object LogicalAnd extends TokenData("&&")
+  case object SquareBracketOpen extends TokenData("[")
+  case object SquareBracketClosed extends TokenData("]")
+  case object CurlyBraceOpen extends TokenData("{")
+  case object CurlyBraceClosed extends TokenData("}")
+  case object LogicalOr extends TokenData("||")
 
-  case class Identifier(value: String) extends TokenData
-  case class IntegerLiteral(value: Int) extends TokenData
+  case class Identifier(override val literal: String) extends TokenData(literal)
+  case class IntegerLiteral(value: Int) extends TokenData(value.toString)
 
   // List of unused, but recognized miniJava keywords:
   // abstract, assert, break, byte, case, catch, char, const, continue, default, double, do, enum, extends, finally,
@@ -58,7 +58,7 @@ object TokenData {
   //
   // List of unused, but recognized miniJava operators:
   // *= ++ += -= -- /= : <<= << >>= >>>= >>> >> ? %= &= & ^= ^ ~ |
-  case class UnusedFeature(value: String) extends TokenData
+  case class UnusedFeature(override val literal: String) extends TokenData(literal)
 
 }
 
