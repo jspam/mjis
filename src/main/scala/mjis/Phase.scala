f45f86f92d5101776a090a61b5e4bcb106047137
Simon Ochsenreither <simon@ochsenreither.de>
2014-11-04 18:07:35 +0100

    Style police: Phase[ResultType] -> Phase[O]
    It is recommended that type variables use a single letter, so that it
    is possible to tell type variables and type instantiations apart.

--- src/main/scala/mjis/Phase.scala
+++ src/main/scala/mjis/Phase.scala
@@ -2,15 +2,15 @@ package mjis
 
 /** Describes the result of a single compiler phase.
   *
-  * If ResultType is an iterator, findings will only be reported up to the current position.
+  * If O is an iterator, findings will only be reported up to the current position.
   */
-trait Phase[ResultType] {
-  protected def getResult(): ResultType
+trait Phase[O] {
+  protected def getResult(): O
   def dumpResult(): Iterator[String]
   lazy val result = getResult()
   def findings: List[Finding]
 }
 
-trait AnalysisPhase[ResultType] extends Phase[ResultType] {
+trait AnalysisPhase[O] extends Phase[O] {
   def success: Boolean = findings.forall(_.severity != Severity.ERROR)
 }
