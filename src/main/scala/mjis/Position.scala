230e2b1bdc3ed8a92fac63662d4626311f336a07
jspam <jspam@users.noreply.github.com>
2014-11-08 17:24:32 +0100

    Strip existing line breaks from lineContents

--- src/main/scala/mjis/Position.scala
+++ src/main/scala/mjis/Position.scala
@@ -2,6 +2,6 @@ package mjis
 
 class Position(val line: Int, val column: Int, val lineContents: String) {
   override def toString: String = s"$line:$column"
-  def longString = lineContents + System.lineSeparator() +
+  def longString = lineContents.takeWhile(c => c != '\r' && c != '\n') + System.lineSeparator() +
     lineContents.take(column-1).map(c => if (c.isWhitespace) c else ' ').mkString + "^"
 }
