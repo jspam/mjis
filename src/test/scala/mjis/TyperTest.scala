6fc03f8419e5d8e20166b7972b6984d84a9b0fad
jspam <jspam@users.noreply.github.com>
2014-11-20 22:35:37 +0100

    Replace TypeBasic(...) with type constants

--- src/test/scala/mjis/TyperTest.scala
+++ src/test/scala/mjis/TyperTest.scala
@@ -33,20 +33,20 @@ class TyperTest extends FlatSpec with Matchers with Inspectors {
   }
 
   "The typer" should "type literals correctly" in {
-    expr("true") should succeedTypingWith(TypeBasic("boolean"))
-    expr("false") should succeedTypingWith(TypeBasic("boolean"))
-    expr("42") should succeedTypingWith(TypeBasic("int"))
-    expr("null") should succeedTypingWith(TypeBasic("null"))
+    expr("true") should succeedTypingWith(BooleanType)
+    expr("false") should succeedTypingWith(BooleanType)
+    expr("42") should succeedTypingWith(IntType)
+    expr("null") should succeedTypingWith(NullType)
   }
 
   it should "type NewArrayExpressions correctly" in {
-    expr("new int[1]") should succeedTypingWith(TypeArray(TypeBasic("int"), 1))
-    expr("new boolean[2][][]") should succeedTypingWith(TypeArray(TypeBasic("boolean"), 3))
+    expr("new int[1]") should succeedTypingWith(TypeArray(IntType, 1))
+    expr("new boolean[2][][]") should succeedTypingWith(TypeArray(BooleanType, 3))
   }
 
   it should "check that the dimensions in NewArrayExpressions are Integers" in {
-    expr("new int[true][]") should failTypingWith(InvalidTypeError(TypeBasic("int"), TypeBasic("boolean")))
-    expr("new boolean[null][]") should failTypingWith(InvalidTypeError(TypeBasic("int"), TypeBasic("null")))
+    expr("new int[true][]") should failTypingWith(InvalidTypeError(IntType, BooleanType))
+    expr("new boolean[null][]") should failTypingWith(InvalidTypeError(IntType, NullType))
   }
 
   it should "disallow void everywhere but in method declarations" in {
@@ -74,14 +74,14 @@ class TyperTest extends FlatSpec with Matchers with Inspectors {
 
   it should "check whether the condition of an If statement is a Boolean expression" in {
     stmt("if (true);") should succeedTyping
-    stmt("if (null);") should failTypingWith(InvalidTypeError(TypeBasic("boolean"), TypeBasic("null")))
-    stmt("if (42);") should failTypingWith(InvalidTypeError(TypeBasic("boolean"), TypeBasic("int")))
+    stmt("if (null);") should failTypingWith(InvalidTypeError(BooleanType, NullType))
+    stmt("if (42);") should failTypingWith(InvalidTypeError(BooleanType, IntType))
   }
 
   it should "check whether the condition of a While statement is a Boolean expression" in {
     stmt("while (true);") should succeedTyping
-    stmt("while (null);") should failTypingWith(InvalidTypeError(TypeBasic("boolean"), TypeBasic("null")))
-    stmt("while (42);") should failTypingWith(InvalidTypeError(TypeBasic("boolean"), TypeBasic("int")))
+    stmt("while (null);") should failTypingWith(InvalidTypeError(BooleanType, NullType))
+    stmt("while (42);") should failTypingWith(InvalidTypeError(BooleanType, IntType))
   }
 
   it should "type check initializers of local variables" in {
