9ea9bef1538fc9dd81dbad13ec29fb1353de0e4c
Max Wagner <max@trollbu.de>
2014-11-22 01:40:20 +0100

    "Constant time" symbol table
    Actually, it's amortized constant time

--- src/main/scala/mjis/SymbolTable.scala
+++ src/main/scala/mjis/SymbolTable.scala
@@ -1,25 +1,33 @@
 package mjis
 
 import mjis.ast.Decl
-
 import scala.collection.mutable
 
 class SymbolTable {
   private class Scope(val parent: Option[Scope]) {
-    val defs = mutable.HashMap[String, Decl]()
-
-    def lookup(name: String): Option[Decl] = defs.get(name).orElse(parent.flatMap(_.lookup(name)))
+    // name -> (<declaration>, <scope of previous declaration>)
+    val defs = mutable.HashMap[String, (Decl, Option[Scope])]()
   }
 
+  private val index = mutable.HashMap[String, Scope]()
   private var current: Option[Scope] = None
 
-  def lookup(name: String): Option[Decl] = current.flatMap(_.lookup(name))
+  def lookup(name: String): Option[Decl] = index.get(name).flatMap(_.defs.get(name)).map(_._1)
 
-  def insert(decl: Decl) = current.get.defs += (decl.name -> decl)
+  def insert(decl: Decl): Unit = {
+    current.get.defs += (decl.name -> (decl, index.get(decl.name)))
+    index += (decl.name -> current.get)
+  }
 
   def enterScope(): Unit = current = Some(new Scope(current))
 
-  def leaveScope(): Unit = current = current.get.parent
+  def leaveScope(): Unit = {
+    current.get.defs.foreach {
+      case (name, (_, Some(oldscope))) => index += (name -> oldscope)
+      case (name, (_, None)) => index.remove(name)
+    }
+    current = current.get.parent
+  }
 
   def inCurrentScope(name: String): Boolean = current.exists(_.defs.contains(name))
 }
