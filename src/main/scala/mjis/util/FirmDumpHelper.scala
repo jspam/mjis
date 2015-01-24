58937723458c9cd47834fc4c9c8dca19e9daaeee
jspam <jspam@users.noreply.github.com>
2015-01-24 17:42:14 +0100

    FirmDumpHelper: Dump node index in addition to node number

--- src/main/scala/mjis/util/FirmDumpHelper.scala
+++ src/main/scala/mjis/util/FirmDumpHelper.scala
@@ -9,6 +9,7 @@ object FirmDumpHelper {
   def dumpGraph(graph: Graph, suffix: String) = {
     new File("./firm-dumps").mkdir() // will silently fail if the directory already exists
     firm.bindings.binding_irdump.ir_set_dump_path("./firm-dumps")
+    Dump.addDumpFlags(firm.bindings.binding_irdump.ir_dump_flags_t.ir_dump_flag_idx_label.`val`)
     Dump.dumpGraph(graph, suffix)
   }
 
